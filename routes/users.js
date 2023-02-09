2104ddaf6b1d276ae74ba69d4145f522a5de9b61
Sara Majeed <smajeed434@gmail.com>
2023-02-09 22:37:10 +0000

    move id param to carts router

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,9 @@ const {
   deleteUserByIdController,
 } = require("../controllers/users");
 
-const { getUserById } = require('../helpers/users');
+
 
 const userRouter = Router();
-const ordersRouter = require("./orders");
 
 module.exports = (app) => {
   app.use("/users", userRouter);
@@ -22,18 +21,6 @@ module.exports = (app) => {
 
   userRouter.delete("/:id", deleteUserByIdController);
 
-  userRouter.param("id", async (req, res, next, id) => {
-    try {
-      let user = await getUserById(id);
-      if (user) {
-        req.user = user.id;
-        next();
-      } else {
-        next(new Error("User does not exist!"));
-      }
-    } catch (err) {
-      next(err);
-    }
-  });
+  
 
 };
